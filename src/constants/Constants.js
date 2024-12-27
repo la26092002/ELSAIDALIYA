6ff1fcf2cbb7c5f4aaa7fe6490e8476818158b5c
la26092002 <larbibenyakhou@gmail.com>
2024-12-27 23:04:53 +0100

    new

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,2 @@
 export const jsonPlaceholderData = "https://jsonplaceholder.typicode.com/users";
-export const URL = "http://localhost:5000";
+export const URL = "https://pharmacy-hfsv.onrender.com";
